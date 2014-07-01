fe52c160154a004b51dc00cf584cccadf9e219eb
James Halliday <mail@substack.net>
2014-07-01 00:04:45 -0700

    passing fail color test

--- test/colors.js
+++ test/colors.js
@@ -30,6 +30,29 @@ test('default pass color', function (t) {
     });
 });
 
+test('default fail color', function (t) {
+    t.plan(2);
+    
+    var green = code('green');
+    var red = code('red');
+    
+    var th = test.createHarness();
+    th.createStream()
+        .pipe(colorize())
+        .pipe(concat(function (body) {
+            var str = body.toString('utf8');
+            t.equal(str.match(RegExp(quotemeta(green), 'g')).length, 1);
+            t.equal(str.match(RegExp(quotemeta(red), 'g')).length, 1);
+        }))
+    ;
+    
+    th(function (tt) {
+        tt.plan(2);
+        tt.equal(1+1, 2);
+        tt.deepEqual([1,2,3], [1,3,2]);
+    });
+});
+
 function code (s) {
     var c = x256(hexCode(s).match(/\w{2}/g).map(fromHex));
     return '\x1b[38;5;' + c;
