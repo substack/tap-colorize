d1b9024ce364dab79896cc7b45f656c50c76c958
Denis Sokolov <denis@sokolov.cc>
2015-05-12 18:22:09 +0300

    Report test failure in exit code

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,6 +3,7 @@
 var colorize = require('../');
 var fs = require('fs');
 var minimist = require('minimist');
+var through = require('through2');
 
 var argv = minimist(process.argv.slice(2), {
     alias: {
@@ -31,4 +32,19 @@ var output = argv.outfile === '-'
     ? process.stdout
     : fs.createWriteStream(argv.outfile)
 ;
-input.pipe(colorize(argv)).pipe(output);
+
+var failures = false;
+input
+    .pipe(through(function(chunk, enc, callback){
+        var line = chunk.toString('utf-8');
+        if (/^not ok\s/.test(line)){
+            failures = true;
+        }
+        callback(null, chunk);
+    }))
+    .pipe(colorize(argv))
+    .on('end', function(){
+        // Report if any tests failed
+        process.exit(failures ? 1 : 0);
+    })
+    .pipe(output);
